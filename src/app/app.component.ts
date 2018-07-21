588809e8d5e505617ded3a4ec9f0347fa95a4437
jpdelopes <joaopedrodelopes@gmail.com>
2018-07-22 00:28:30 +0100

    Finished Course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+// Learnt from: https://www.youtube.com/watch?v=3qmOFLBhfD8
+
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  items: FirebaseListObservable<any>;
+  name: any;
+  msgVal: string = '';
+
+  constructor(public af: AngularFire) {
+    this.items = af.database.list('/messages', {
+      query: {
+        limitToLast: 5
+      }
+    });
+    this.af.auth.subscriber(auth => {
+      if(auth) {
+        this.name = auth;
+      }
+    });
+  }
+
+  login() {
+    this.af.auth.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Popup // .Redirect
+    })
+  }
+
+  chatSend(theirMessage: string) {
+    //Retrieving from the app.component.html file
+    this.items.push({ message: theirMessage, name: this.name.facebook.displayName});
+    this.msgVal = '';
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+// Learnt from: https://www.youtube.com/watch?v=3qmOFLBhfD8
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  //Removed for safety reasons
+  
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
